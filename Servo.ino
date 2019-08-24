c34675ca89f631968ca73a83f91c8648b0b23fae
MBurakBaytok <54472106+MBurakBaytok@users.noreply.github.com>
2019-08-24 15:31:44 +0300

    Add files via upload

---
+++ Servo.ino
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include<Servo.h>
+char serial;
+Servo motor;
+int i=90;
+void setup() {
+  Serial.begin(9600);
+  motor.attach(6);
+}
+
+
+void loop() 
+{
+  if(Serial.available())
+  {
+    serial=Serial.read();
+    Serial.println(serial,HEX);
+    if(serial=='R')
+    {
+      motor.write(i);
+      i++;
+      delay(2);
+     }
+    if(serial=='S')
+    {
+      motor.write(i);
+    }
+    if(serial=='L')
+    {
+      motor.write(i);
+      i--;
+      delay(2);
+    }
+  }
+  }
